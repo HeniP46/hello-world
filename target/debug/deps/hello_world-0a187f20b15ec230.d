493c76e96440ca63d52a6e46db8000f2356dc879
Heni Patel <henipatel@henis-mbp.myfiosgateway.com>
2025-09-18 21:17:54 -0400

    Initial Rust Hello World

---
+++ target/debug/deps/hello_world-0a187f20b15ec230.d
@@ -0,0 +1,5 @@
+/Users/henipatel/hello-world/target/debug/deps/hello_world-0a187f20b15ec230.d: src/main.rs
+
+/Users/henipatel/hello-world/target/debug/deps/hello_world-0a187f20b15ec230: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/henipatel/hello-world/target/debug/hello-world: /Users/henipatel/hello-world/src/main.rs
